8ec65f2edc3ac3f59772c4c49880fac42fe160f3
chinmay-5 <66941351+chinmay-5@users.noreply.github.com>
2020-06-16 16:25:52 +0530

    Added thrust sound effect

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;    
+    Rigidbody rigidBody;
+    AudioSource audioSource;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }     
         }
-        
+        else
+        {
+            audioSource.Stop();
+        }
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
